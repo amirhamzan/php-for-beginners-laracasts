08643d4cd2d4de6d7274f44b98c8b05ed088931f
Amir Hassan <amirhassan.hamzan@gmail.com>
2023-02-16 06:03:51 +0800

    Create basic index.php

---
+++ demo/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+
+<body>
+    <!-- to run devlopment on server, run 'php -S localhost:<port number>'-->
+    <h1>hello world using native html</h1>
+
+    <h1>
+        <?php
+        echo "hello world using php echo";
+        ?>
+    </h1>
+</body>
+
+</html>
